d875c99d46c490d2cb7ee6ccb874e89432cd5b69
chetanjoshi <chetan.joshi@divinetalk.in>
2024-12-29 01:08:04 +0530

    fixed rings

--- components/objects/hero_rings.tsx
+++ components/objects/hero_rings.tsx
@@ -3,10 +3,10 @@ import "../../app/globals.css"
 function HeroRings(){
     return ( <div>
       <div className="absolute inset-0 -z-30 opacity-5" style={{backgroundImage:`url("/images/objects/grain.jpg")`}}></div>
-        <div className="height-[800px] width-[400px] hero-ring"></div>
-        <div className="size-[820] hero-ring"></div>
-        <div className="size-[420] hero-ring"></div>
-        <div className="size-[220] hero-ring"></div>
+        <div className="h-[600px] w-[600px] opacity-[0.9] hero-ring"></div>
+        <div className="h-[800px] w-[800px] opacity-[0.4] hero-ring"></div>
+        <div className="h-[400px] w-[400px] hero-ring"></div>
+        <div className="h-[200px] w-[200px] hero-ring"></div>
     </div>)
   }
 
