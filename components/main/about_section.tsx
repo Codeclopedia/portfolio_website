07c27ef1dd81a9c15265aa21c47f103388ce7a54
chetanjoshi <chetan.joshi@divinetalk.in>
2025-01-02 18:38:04 +0530

    apos error fixed

--- components/main/about_section.tsx
+++ components/main/about_section.tsx
@@ -18,11 +18,11 @@ export default function About() {
           />
         </div>
         {selectedIndex === 0 ? <WorKSection /> : <div className="mt-10 py-10 h-min flex flex-col items-center justify-center bg-slate-800 text-center">
-  <div className="font-bold text-[100px] text-white -m-8">
+  <div className="font-bold text-[30px] sm:text-[100px] text-white -m-8">
     COMING SOON
   </div>
-  <div className="items-center text-white text-lg ">
-    Good things take time. And I&asop;m trying my best to get it done ASAP.
+  <div className="items-center text-white text-sm sm:text-lg ">
+    Good things take time. And I&apos;m trying my best to get it done ASAP.
   </div>
 </div>
 
